ba0993b47f66edb6a425e32da68a388c77890d04
riyal1234 <99786117+riyal1234@users.noreply.github.com>
2025-12-01 10:39:10 +0530

    first commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  // Provided static Agora credentials
+  // Provided static git remote add origin https://github.com/riyallll/Agora Video call App.git credentials
   static const String agoraAppId = "c67333a5a95742dc97b89b3c5f555322";
   static const String channelName = "testChannel";
   static const String agoraToken = "007eJxTYGj4vYrnc+nxoNAze1M7/m0IYpt71mZXTsl3YaW8gFrfxPkKDMlm5sbGxommiZam5iZGKcmW5kkWlknGyaZppqamxkZGD2V0MxsCGRleKDiyMDJAIIjPzVCSWlzinJGYl5eaw8AAAG3OIgI=";
